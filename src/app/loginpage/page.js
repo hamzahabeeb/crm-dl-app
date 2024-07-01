29575421f50b79b209c4b7422057e717bc68e6bd
Hamza Habeeb <habeebhamza@yahoo.com>
2024-07-01 18:07:50 +0530

    login page and registration

--- src/app/loginpage/page.js
+++ src/app/loginpage/page.js
@@ -1,56 +1,96 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    router.push("/registration");
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get("http://localhost:8000/users", {
+        username,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   return (
     <div className="flex">
       <div className="w-1/2 bg-[#F4F6F9] flex flex-col justify-center content-center">
         <div className="px-20 content-center">
           <div className="px-40 mx-12">
-            <img src="https://www.digital-lync.com/images/digital-lyncs-devops-python-digital-marketing-training-hyderabad.png" width="150"></img>
+            <img
+              src="https://www.digital-lync.com/images/digital-lyncs-devops-python-digital-marketing-training-hyderabad.png"
+              width="150"
+            ></img>
           </div>
           <div className="container h-[21rem] w-[25rem] border rounded border-gray-300 bg-white mx-24 mt-10">
             <div className="p-8">
-              <div>
-                <p className="text-gray-500 text-xs pb-1">User Name</p>
-              </div>
-              <div className="border border-black-800 rounded-md h-9  hover:border-blue-700">
-                <input
-                  type="text"
-                  className="w-full h-full bg-transparent border-none outline-none text-black pl-1"
-                ></input>
-              </div>
-              <div>
-                <p className="text-gray-500 text-xs pb-1 pt-4">Password</p>
-              </div>
-              <div className="border border-black-800 rounded-md h-9  hover:border-blue-700">
-                <input
-                  type="password"
-                  className="w-full h-full bg-transparent border-none outline-none text-black pl-1"
-                ></input>
-              </div>
-              <div className="pt-4">
-                <button
-                  className="w-full bg-blue-500 hover:bg-blue-700 text-white text-sm font-normal py-2 my-3 px-4 rounded"
-                  type="submit">
-                  Login
-                </button>
-              </div>
-              <div className="flex items-center pt-2">
+              <form onSubmit={handleSubmit}>
+                <div className="text-gray-500 text-xs pb-1">
+                  <label>User Name</label>
+                </div>
+                <div className="border border-black-800 rounded-md h-9  hover:border-blue-700">
+                  <input
+                    type="text"
+                    className="w-full h-full bg-transparent border-none outline-none text-black pl-1"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  ></input>
+                </div>
+                <div className="text-gray-500 text-xs pb-1 pt-4">
+                  <label>Password</label>
+                </div>
+                <div className="border border-black-800 rounded-md h-9  hover:border-blue-700">
+                  <input
+                    type="password"
+                    className="w-full h-full bg-transparent border-none outline-none text-black pl-1"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></input>
+                </div>
+                <div className="pt-4">
+                  <button
+                    className="w-full bg-blue-500 hover:bg-blue-700 text-white text-sm font-normal py-2 my-3 px-4 rounded"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                </div>
+                <div className="flex items-center pt-2">
                   <input
                     type="checkbox"
                     className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 cursor-pointer"
                   />
-                  <label className="ml-2 text-xs text-gray-600">Remember me</label>
+                  <label className="ml-2 text-xs text-gray-600">
+                    Remember me
+                  </label>
                 </div>
                 <hr className="my-4" />
-              <div className="text-xs text-gray-600 hover:text-blue-700 cursor-pointer">Forgot password?</div>
+                <div className="text-xs text-gray-600 hover:text-blue-700 cursor-pointer">
+                  Forgot password?
+                </div>
+              </form>
             </div>
           </div>
-          <div className="text-blue-700 text-center my-5 text-xs hover:text-blue-900 cursor-pointer	">Dont have an account?</div>
-          <div></div>
+          <div className="text-blue-700 text-center my-5 text-xs hover:text-blue-900 cursor-pointer	">
+            <button onClick={handleRegister}>Dont have an account?</button>
+          </div>
         </div>
       </div>
       <div className="w-1/2">
-        <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat">
+        <div className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat">
           <img src="https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg" />
         </div>
       </div>
---
+++ src/app/registration/page.js
@@ -0,0 +1,161 @@
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export default function Registration() {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      console.log(setError);
+    } else {
+      try {
+        const response = await axios.post("http://localhost:8000/users", {
+          name: name,
+          username: username,
+          email: email,
+          password: password,
+        });
+        console.log(response);
+        router.push("/loginpage");
+      } catch (error) {
+        setError(error.response.data.message);
+        console.log(setError);
+      }
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-[#F4F6F9] flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className=" sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-5  text-gray-700"
+              >
+                Name
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <input
+                  id="name"
+                  name="name"
+                  placeholder="John Doe"
+                  type="text"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className=" text-black appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                />
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-5 text-gray-700"
+              >
+                Username
+              </label>
+              <div className="mt-1 flex rounded-md shadow-sm">
+                <input
+                  id="username"
+                  name="username"
+                  placeholder="john"
+                  type="text"
+                  required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="text-black appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 "
+                />
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-5  text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <input
+                  id="email"
+                  name="email"
+                  placeholder="user@example.com"
+                  type="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="text-black appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                "
+                />
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-5 text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1 rounded-md shadow-sm">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="text-black appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                />
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <label
+                htmlFor="password_confirmation"
+                className="block text-sm font-medium leading-5 text-gray-700"
+              >
+                Confirm Password
+              </label>
+              <div className="mt-1 rounded-md shadow-sm">
+                <input
+                  id="password_confirmation"
+                  name="password_confirmation"
+                  type="password"
+                  required
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  className=" text-black appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                />
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <span className="block w-full rounded-md shadow-sm">
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                >
+                  Register
+                </button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
